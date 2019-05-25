a1c5af421160c447745dc301fa639a9472c532a7
Pumuckl007 <maximilian.paulsen@gmail.com>
2019-05-25 10:34:48 -0700

    Changed the ECUTransciver to an SCM transciver as there was no ECU specific code

--- src/org/rocketproplab/marginalstability/flightcomputer/comm/SCMTransceiver.java
+++ src/org/rocketproplab/marginalstability/flightcomputer/comm/SCMTransceiver.java
@@ -5,34 +5,38 @@ import org.rocketproplab.marginalstability.flightcomputer.events.SerialListener;
 import org.rocketproplab.marginalstability.flightcomputer.hal.SerialPort;
 
 /**
- * A class to handle the sending and receiving information from the ECU serial
- * port.
+ * A class to handle the sending and receiving information from the any SCM
+ * source.
  * 
  * @author Max Apodaca, Antonio
  *
  */
-public class ECUTransceiver
+public class SCMTransceiver
     implements SerialListener, PacketListener<SCMPacket> {
-  private SerialPort   serialPort;
-  private PacketRouter router;
+  private SerialPort    serialPort;
+  private PacketRouter  router;
+  private PacketSources source;
 
   /**
-   * Create a new ECU Transceiver that will use this serial port to send and
+   * Create a new SCM Transceiver that will use this serial port to send and
    * receive data.
    * 
    * @param serialPort The serial port to send data to
    * @param router     the router to use to route packets
+   * @param source     where the SCM is connected to
    */
-  public ECUTransceiver(SerialPort serialPort, PacketRouter router) {
+  public SCMTransceiver(SerialPort serialPort, PacketRouter router,
+      PacketSources source) {
     this.serialPort = serialPort;
     this.router     = router;
+    this.source     = source;
   }
 
   @Override
   public void onSerialData(String data) {
     SCMPacket packet = new SCMPacket(data);
     if (packet.isValid()) {
-      router.recivePacket(packet, PacketSources.EngineControllerUnit);
+      router.recivePacket(packet, this.source);
     } else {
       System.out.println("Got invalid packet " + data + "!");
       // TODO report error
--- test/org/rocketproplab/marginalstability/flightcomputer/comm/TestSCMTransceiver.java
+++ test/org/rocketproplab/marginalstability/flightcomputer/comm/TestSCMTransceiver.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import org.rocketproplab.marginalstability.flightcomputer.events.SerialListener;
 import org.rocketproplab.marginalstability.flightcomputer.hal.SerialPort;
 
-public class TestECUTransceiver {
+public class TestSCMTransceiver {
 
   private class TestSerialPort implements SerialPort {
 
@@ -30,35 +30,64 @@ public class TestECUTransceiver {
   }
 
   @Test
-  public void testECUTransciverRedirectsRecievedPacketToRouter() {
+  public void testSCMTransciverRedirectsRecievedPacketToRouter() {
     PacketRouter                  router   = new PacketRouter();
     TestSerialPort                port     = new TestSerialPort();
-    ECUTransceiver                tx       = new ECUTransceiver(port, router);
+    
+    SCMTransceiver                tx       = new SCMTransceiver(port, router,
+        PacketSources.EngineControllerUnit);
+    
     TestPacketListener<SCMPacket> listener = new TestPacketListener<SCMPacket>();
+    
     router.addListener(listener, SCMPacket.class,
         PacketSources.EngineControllerUnit);
     tx.onSerialData(new SCMPacket(SCMPacketType.VS, "10010").toString());
+    
     SCMPacket comparePacket = new SCMPacket(
         new SCMPacket(SCMPacketType.VS, "10010").toString());
+    
     assertEquals(comparePacket, listener.lastPacket);
     assertEquals(0, port.lastWritten.size());
   }
 
   @Test
-  public void testECUTransciverRedirectsRecivedPacketToPort() {
+  public void testSCMTransciverRedirectsRecivedPacketToPort() {
     PacketRouter                  router   = new PacketRouter();
     TestSerialPort                port     = new TestSerialPort();
-    ECUTransceiver                tx       = new ECUTransceiver(port, router);
+    
+    SCMTransceiver                tx       = new SCMTransceiver(port, router,
+        PacketSources.EngineControllerUnit);
+    
     TestPacketListener<SCMPacket> listener = new TestPacketListener<SCMPacket>();
+    
     router.addListener(listener, SCMPacket.class,
         PacketSources.EngineControllerUnit);
     router.addListener(tx, SCMPacket.class, PacketSources.EngineControllerUnit);
     SCMPacket sourcePacket = new SCMPacket(
         new SCMPacket(SCMPacketType.VS, "10010").toString());
     router.sendPacket(sourcePacket, PacketSources.EngineControllerUnit);
+    
     assertEquals(sourcePacket.toString(), port.lastWritten.get(0));
     assertEquals(1, port.lastWritten.size());
     assertEquals(listener.lastDirection, PacketDirection.SEND);
+  }
+
+  @Test
+  public void testSCMTransciverRedirectsPacketBasedOnSource() {
+    PacketRouter                  router   = new PacketRouter();
+    TestSerialPort                port     = new TestSerialPort();
+    
+    SCMTransceiver                tx       = new SCMTransceiver(port, router,
+        PacketSources.CommandBox);
+    
+    TestPacketListener<SCMPacket> listener = new TestPacketListener<SCMPacket>();
+    
+    router.addListener(listener, SCMPacket.class,
+        PacketSources.EngineControllerUnit);
+    tx.onSerialData(new SCMPacket(SCMPacketType.VS, "10010").toString());
+    
+    assertEquals(null, listener.lastPacket);
+    assertEquals(0, port.lastWritten.size());
   }
 
 }
