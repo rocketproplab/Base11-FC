fccab7d0b9be40446c6dc55bc46878d28e65db95
Pumuckl007 <maximilian.paulsen@gmail.com>
2020-03-07 11:55:26 -0800

    Added Error reporting system

---
+++ test/org/rocketproplab/marginalstability/flightcomputer/FlightComputerTest.java
@@ -0,0 +1,76 @@
+package org.rocketproplab.marginalstability.flightcomputer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.rocketproplab.marginalstability.flightcomputer.subsystems.Subsystem;
+import org.rocketproplab.marginalstability.flightcomputer.subsystems.Telemetry;
+
+public class FlightComputerTest {
+  private Telemetry telemetry;
+  private ArrayList<Errors> errorList;
+  
+  private class MockSubsystem implements Subsystem {
+    public boolean hasUpdateCalled = false;
+    public boolean throwError = false;
+    
+    @Override
+    public void update() {
+      this.hasUpdateCalled = true;
+      if(this.throwError) {
+        throw new RuntimeException();
+      }
+    }
+  }
+ 
+  
+  @Before
+  public void beforeEach() {
+    errorList = new ArrayList<>();
+    this.telemetry = new Telemetry(Logger.getLogger("Dummy"), null) {
+      @Override
+      public void reportError(Errors error) {
+        errorList.add(error);
+      }
+    };
+  }
+  
+  @Test
+  public void flightComputerCallsSubsystemUpdateOnTick() {
+    FlightComputer flightComputer = new FlightComputer(this.telemetry);
+    MockSubsystem mockSubsystem = new MockSubsystem();
+    flightComputer.registerSubsystem(mockSubsystem);
+    flightComputer.tick();
+    assertTrue(mockSubsystem.hasUpdateCalled);
+  }
+  
+  @Test
+  public void flightComputerRecoversFromException() {
+    FlightComputer flightComputer = new FlightComputer(this.telemetry);
+    MockSubsystem mockSubsystem = new MockSubsystem();
+    mockSubsystem.throwError = true;
+    flightComputer.registerSubsystem(mockSubsystem);
+    flightComputer.tick();
+    mockSubsystem.hasUpdateCalled = false;
+    mockSubsystem.throwError = false;
+    flightComputer.tick();
+    assertTrue(mockSubsystem.hasUpdateCalled);
+  }
+  
+  @Test
+  public void flightComputerReportsErrorToTelemetry() {
+    FlightComputer flightComputer = new FlightComputer(this.telemetry);
+    MockSubsystem mockSubsystem = new MockSubsystem();
+    mockSubsystem.throwError = true;
+    flightComputer.registerSubsystem(mockSubsystem);
+    flightComputer.tick();
+    assertEquals(1, this.errorList.size());
+    assertEquals(Errors.TOP_LEVEL_EXCEPTION, this.errorList.get(0));
+  }
+
+}
