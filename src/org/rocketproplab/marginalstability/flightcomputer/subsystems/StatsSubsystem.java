fe17cc5e240db3d6ba8eb40a3a026116f808badd
Max <maximilian.paulsen@gmail.com>
2020-09-19 12:32:44 -0700

    Added the first class for the stats subsytem

---
+++ src/org/rocketproplab/marginalstability/flightcomputer/subsystems/StatsSubsystem.java
@@ -0,0 +1,13 @@
+package org.rocketproplab.marginalstability.flightcomputer.subsystems;
+
+/**
+ * Subsystem that contains all of the sensors that are only used to provide
+ * statistics, also listens for events to provide statistics.
+ * 
+ * Contains all the Thermo Couple Sensors and Pressure Tranducers
+ * @author Max Apodaca
+ *
+ */
+public class StatsSubsystem {
+
+}
