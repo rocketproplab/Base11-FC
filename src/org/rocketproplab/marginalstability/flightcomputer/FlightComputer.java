b479303119783ad6cf27b420082f525f3f0ad961
Kyros Chow <kyroschow@gmail.com>
2020-10-17 13:37:05 -0700

    Fix FlightComputer tests, FlightComputer.tick() catches reportError exceptions

--- src/org/rocketproplab/marginalstability/flightcomputer/FlightComputer.java
+++ src/org/rocketproplab/marginalstability/flightcomputer/FlightComputer.java
@@ -1,49 +1,39 @@
 package org.rocketproplab.marginalstability.flightcomputer;
 
-import java.util.ArrayList;
-
 import org.rocketproplab.marginalstability.flightcomputer.looper.Looper;
 import org.rocketproplab.marginalstability.flightcomputer.subsystems.Subsystem;
 import org.rocketproplab.marginalstability.flightcomputer.subsystems.Telemetry;
 
 public class FlightComputer {
-    private Telemetry telemetry;
-    private Time      time;
-    private Looper    looper;
+  private Telemetry telemetry;
+  private Time      time;
+  private Looper    looper;
 
 
-    public FlightComputer(Telemetry telemetry, Time time) {
-        this.telemetry = telemetry;
-        this.time = time;
-        this.looper = new Looper(time);
+  public FlightComputer(Telemetry telemetry, Time time) {
+    this.telemetry = telemetry;
+    this.time      = time;
+    this.looper    = new Looper(time);
+  }
+
+  public void registerSubsystem(Subsystem subsystem) {
+    subsystem.prepare(this.looper);
+  }
+
+  public Time getTime() {
+    return time;
+  }
+
+  public void tick() {
+    try {
+      this.looper.tick();
+    } catch (Exception looperException) {
+      try {
+        this.telemetry.reportError(Errors.TOP_LEVEL_EXCEPTION);
+      } catch (Exception telemetryException) {
+        System.err.println("Unable to log errors!");
+        telemetryException.printStackTrace();
+      }
     }
-
-    public void registerSubsystem(Subsystem subsystem) {
-        subsystem.prepare(this.looper);
-    }
-
-    public Time getTime() {
-        return time;
-    }
-
-    public void tick() {
-        try {
-            this.looper.tick();
-        } catch (Exception e) {
-            this.telemetry.reportError(Errors.TOP_LEVEL_EXCEPTION);
-        }
-//    try {
-//      for(Subsystem subsystem : this.subsystems) {
-//        try {
-//          subsystem.update();
-//        } catch (Exception e) {
-//          this.telemetry.reportError(Errors.TOP_LEVEL_EXCEPTION);
-//        }
-//      }
-//    } catch (Exception e) {
-//      System.err.println("Unable to log errors!");
-//      e.printStackTrace();
-//    }
-
-    }
+  }
 }
