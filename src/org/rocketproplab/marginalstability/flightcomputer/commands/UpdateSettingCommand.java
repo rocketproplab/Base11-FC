22bfa2188c275ed5b9e589595b2410e9b705edab
Auroric <aaronyu330@gmail.com>
2020-11-14 12:09:27 -0800

    Implement command and command factory to update settings configuration, closes #49

---
+++ src/org/rocketproplab/marginalstability/flightcomputer/commands/UpdateSettingCommand.java
@@ -0,0 +1,36 @@
+package org.rocketproplab.marginalstability.flightcomputer.commands;
+
+import org.rocketproplab.marginalstability.flightcomputer.Settings;
+import org.rocketproplab.marginalstability.flightcomputer.subsystems.Subsystem;
+
+import java.util.Collections;
+
+public class UpdateSettingCommand implements Command {
+    private final String setting;
+
+    UpdateSettingCommand(String setting) {
+        this.setting = setting;
+    }
+
+    public static UpdateSettingCommand getUpdateSettingCommand(String setting) {
+        return new UpdateSettingCommand(setting);
+    }
+
+    @Override
+    public boolean isDone() { return true; }
+
+    @Override
+    public void execute() {
+        Settings.readSettingsFromConfig(Collections.singletonList(setting));
+    }
+
+    @Override
+    public void start() { }
+
+    @Override
+    public void end() { }
+
+    @Override
+    public Subsystem[] getDependencies() { return new Subsystem[0]; }
+
+}
