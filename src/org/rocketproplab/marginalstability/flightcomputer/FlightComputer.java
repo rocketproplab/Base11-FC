e4c74d104a0492380be13a76aab56d8cb40da2ca
Chi Chow <cschow@ucsd.edu>
2021-03-06 13:27:35 -0800

    FlightComputer and sensors documentation

--- src/org/rocketproplab/marginalstability/flightcomputer/FlightComputer.java
+++ src/org/rocketproplab/marginalstability/flightcomputer/FlightComputer.java
@@ -10,8 +10,20 @@ import org.rocketproplab.marginalstability.flightcomputer.subsystems.*;
 import java.util.logging.Logger;
 
 public class FlightComputer {
+    /**
+     * CLI argument names and descriptions
+     */
+    private static final String REAL_SENSORS = "real-sensors",
+            REAL_SENSORS_DESC = "use real sensors";
+
     private static FlightComputer instance;
 
+    /**
+     * Creates a FlightComputer instance, which can only be created once.
+     *
+     * @param args arguments to configure FightComputer settings
+     * @return new FlightComputer instance
+     */
     public static FlightComputer create(String[] args) {
         if (instance != null) {
             throw new RuntimeException("FlightComputer has already been created.");
@@ -20,37 +32,68 @@ public class FlightComputer {
         return instance;
     }
 
+    /**
+     * Convenient method to create a FlightComputer instance without any arguments.
+     *
+     * @return new FlightComputer instance
+     */
     public static FlightComputer create() {
         return create(new String[0]);
     }
 
+    /**
+     * Time used by all objects created in the FC.
+     */
     private final Time time;
+
+    /**
+     * Looper used by objects created in the FC, e.g. subsystems.
+     */
     private final Looper looper;
 
     private PacketRouter packetRouter = new PacketRouter();
     private Telemetry telemetry = new Telemetry(Logger.getLogger("Telemetry"), packetRouter);
+
+    /**
+     * Providers to provide singleton objects to higher level management objects.
+     */
     private SensorProvider sensorProvider;
 
-    // TODO: all the arguments
+    /**
+     * Private FlightComputer constructor to avoid multiple initializations.
+     *
+     * @param args arguments to configure FlightComputer settings
+     */
     private FlightComputer(String[] args) {
         this.time = new Time();
         this.looper = new Looper(this.time);
         initWithArgs(args);
     }
 
+    /**
+     * Initialize input/output devices and other settings based on arguments.
+     *
+     * @param args arguments for configuration
+     */
     private void initWithArgs(String[] args) {
-        // TODO: initialize all input/output devices based on arguments
         CommandLine cmd = parseArgs(args);
 
         // use real/fake sensors
-        boolean useRealSensors = cmd.hasOption("real-sensors");
+        boolean useRealSensors = cmd.hasOption(REAL_SENSORS);
         sensorProvider = SensorProvider.create(useRealSensors);
     }
 
+    /**
+     * Defines command line options and possible arguments.
+     * Also prints help and exits if invalid arguments are detected.
+     *
+     * @param args arguments to parse
+     * @return command line options for configuration
+     */
     private CommandLine parseArgs(String[] args) {
         Options options = new Options();
 
-        Option sensorType = new Option(null, "real-sensors", false, "use real sensors");
+        Option sensorType = new Option(null, REAL_SENSORS, false, REAL_SENSORS_DESC);
         options.addOption(sensorType);
 
         CommandLineParser parser = new DefaultParser();
@@ -65,13 +108,20 @@ public class FlightComputer {
         return null;
     }
 
+    /**
+     * Allows subsystems to register events and callbacks with the looper.
+     *
+     * @param subsystem subsystem to register
+     */
     public void registerSubsystem(Subsystem subsystem) {
         subsystem.prepare(this.looper);
     }
 
+    /**
+     * After all input/output devices and settings are initialized, higher level objects can be created.
+     * Use objects provided by the providers, and simply inject all objects needed to create any higher level object.
+     */
     public void initHighLevelObjects() {
-        // TODO: after all input/output devices are initialized, create higher level objects using these basic devices
-        // TODO: use local variables; simply inject all the objects needed to create any higher level object
 
         // PacketRouter
         PacketRouter packetRouter = new PacketRouter();
@@ -111,23 +161,44 @@ public class FlightComputer {
         });
     }
 
+    /**
+     * Builder class to create FlightComputer objects with more customization.
+     */
     public static class Builder {
         private Telemetry telemetry;
         private final FlightComputer fc;
 
+        /**
+         * Creates a new Builder instance, which can be used to customize the FC.
+         *
+         * @param args arguments to configure FlightComputer settings.
+         */
         public Builder(String[] args) {
             fc = FlightComputer.create(args);
         }
 
+        /**
+         * Convenient method to create a Builder instance without any arguments.
+         */
         public Builder() {
             this(new String[0]);
         }
 
+        /**
+         * Set custom telemetry.
+         *
+         * @param telemetry customized telemetry
+         * @return this Builder
+         */
         public Builder withTelemetry(Telemetry telemetry) {
             this.telemetry = telemetry;
             return this;
         }
 
+        /**
+         * Customizations set programmatically in the Builder will override defaults and arguments.
+         * @return customized FlightComputer
+         */
         public FlightComputer build() {
             if (telemetry != null) fc.telemetry = this.telemetry;
             return fc;
--- src/org/rocketproplab/marginalstability/flightcomputer/SensorProvider.java
+++ src/org/rocketproplab/marginalstability/flightcomputer/SensorProvider.java
@@ -1,11 +1,24 @@
 package org.rocketproplab.marginalstability.flightcomputer;
 
+import org.rocketproplab.marginalstability.flightcomputer.hal.LPS22HD;
+
+/**
+ * A provider to provide singleton sensor instances.
+ *
+ * @author Chi Chow
+ */
 public abstract class SensorProvider {
 
-    abstract void getSensor();
+    abstract LPS22HD getPressureSensor();
 
     private static SensorProvider instance;
 
+    /**
+     * Create singleton SensorProvider.
+     *
+     * @param useRealSensors whether to use real sensors or simulators
+     * @return new SensorProvider instance
+     */
     public static SensorProvider create(boolean useRealSensors) {
         if (instance != null) {
             throw new RuntimeException("SensorProvider has already been created.");
@@ -18,18 +31,25 @@ public abstract class SensorProvider {
         return instance;
     }
 
-    // TODO: add all necessary sensors
+    /**
+     * SensorProvider that returns simulators
+     */
     private static class SimulatorProvider extends SensorProvider {
         @Override
-        public void getSensor() {
+        public LPS22HD getPressureSensor() {
             // TODO: implement methods to get different simulators
+            throw new RuntimeException("Not implemented!");
         }
     }
 
+    /**
+     * SensorProvider that returns real sensors
+     */
     private static class RealSensorProvider extends SensorProvider {
         @Override
-        public void getSensor() {
+        public LPS22HD getPressureSensor() {
             // TODO: implement methods to get different real sensors
+            throw new RuntimeException("Not implemented!");
         }
     }
 }
